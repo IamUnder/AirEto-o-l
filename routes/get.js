0b5a5211c473efeeb08d2073888cee7a2bc937a4
Under <jorgeolmo.I@gmail.com>
2022-05-02 20:26:01 +0200

    Modificación de las rutas de get generales

--- routes/get.js
+++ routes/get.js
@@ -7,7 +7,9 @@ const get = require('../controllers/get')
 
 
 // Rutas
-router.get('/now', get.get) // Ruta de testing
+router.get('/now', get.get) // Ruta para obtener las predicciones de esta semana
+router.get('/price', get.price) // Ruta para obtener los premios de esta semana
+router.get('/price/last', get.priceLastWeek) // Ruta para obtener los premios de la semana pasada
 
 // Exportacion de modulos
 module.exports = router
